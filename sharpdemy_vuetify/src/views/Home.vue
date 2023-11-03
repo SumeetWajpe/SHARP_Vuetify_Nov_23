0093d246298f5be04fa80f65ee3a354778e553ef
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-11-03 10:27:12 +0530

    ✨ column sizes based on media breakpoints

--- sharpdemy_vuetify/src/views/Home.vue
+++ sharpdemy_vuetify/src/views/Home.vue
@@ -8,8 +8,8 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
-        <v-card class="mx-auto" max-width="400">
+      <v-col xs="12" sm="6" md="4">
+        <v-card class=" mx-auto" max-width="400">
           <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
             <v-card-title>Top 10 Australian beaches</v-card-title>
           </v-img>
@@ -36,7 +36,61 @@
         </v-card>
 
       </v-col>
-      <v-col>
+      <v-col xs="12" sm="6" md="4">
+        <v-card class="mx-auto" max-width="400">
+          <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pt-4">
+            Number 10
+          </v-card-subtitle>
+
+          <v-card-text>
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange">
+              Share
+            </v-btn>
+
+            <v-btn color="orange">
+              Explore
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col xs="12" sm="6" md="4">
+        <v-card class="mx-auto" max-width="400">
+          <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+
+          <v-card-subtitle class="pt-4">
+            Number 10
+          </v-card-subtitle>
+
+          <v-card-text>
+            <div>Whitehaven Beach</div>
+
+            <div>Whitsunday Island, Whitsunday Islands</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="orange">
+              Share
+            </v-btn>
+
+            <v-btn color="orange">
+              Explore
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col xs="12" sm="6" md="4">
         <v-card class="mx-auto" max-width="400">
           <v-img class="align-end text-white" height="200" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg" cover>
             <v-card-title>Top 10 Australian beaches</v-card-title>
